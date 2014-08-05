fc364b8ddc4af3c3aa5ac774831b876731c53029
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2014-08-05 12:09:14 -0400

    frontpage: second commit: fixed ActionLink
    fixed ActionLink to have more fluidity

--- libs/ActionLink.php
+++ libs/ActionLink.php
@@ -2,8 +2,12 @@
 
 
 class ActionLink{
-    public static function create($text,$controller,$action="",$param="",$class="", $id=""){
-        echo "<a href='$controller/$action".($param==""?"":"/$param")."' class=$class id=$id>$text</a>";
+    public static function create($link, $controller, $action = NULL, $param = NULL, $query = NULL, $class = "", $id = ""){
+        $action = $action ? "/$action" : "";
+        $param = $param ? ($action ? "/$param" : "/index/$param") : "";
+        $query = $query ? "?".http_build_query($query) : "";
+
+        echo "<a href='$controller$action$param$query' class='$class' id='$id'>$link</a>";
     }
 }
 ?>
--- views/master.php
+++ views/master.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
-        <link href="public/css/master.css" type="text/css" rel="stylesheet">
-        <?php for($i=0; $i<sizeof($this->css);$i++):?>
-        <link href="views/<?php echo $controller."/css/".$this->css[$i].".css";?>" type="text/css" rel="stylesheet">
-        <?php endfor;?>
-        <?php for($i=0; $i<sizeof($this->js);$i++):?>
-        <script src="views/<?php echo $controller."/js/".$this->js[$i].".js";?>"></script>
-        <?php endfor;?>
-
-
-
         <title>TestBook <?php echo isset($this->title)?"| ".$this->title:""; ?></title>
+        <link href="public/css/master.css" type="text/css" rel="stylesheet">
+
+        <?php for($i=0; $i<sizeof($this->css);$i++): ?>
+        <link href="views/<?php echo $controller."/css/".$this->css[$i].".css";?>" type="text/css" rel="stylesheet">
+        <?php endfor;
+              for($i=0; $i<sizeof($this->js);$i++): ?>
+        <script src="views/<?php echo $controller."/js/".$this->js[$i].".js";?>"></script>
+        <?php endfor; ?>
     </head>
     <body>
         <ul class="menu">
-            <li> <?php ActionLink::create("Profile","Profile","","$this->id","item");?></li>
-            <li> <?php ActionLink::create("Posts","Profile","posts","$this->id","item");?></li>
-            <li> <?php ActionLink::create("Photos","Profile","photos","$this->id","item");?></li>
-            <li> <?php ActionLink::create("Friends","Profile","friends","$this->id","item");?></li>
-            <li> <?php ActionLink::create("Settings","Profile","settings","$this->id","item");?></li>
+            <li> <?php ActionLink::create("Profile", "Profile", NULL, NULL, array("id" => $this->id), "item");?></li>
+            <li> <?php ActionLink::create("Posts", "Profile", "posts", NULL, array("id" => $this->id), "item");?></li>
+            <li> <?php ActionLink::create("Photos", "Profile", "photos", NULL, array("id" => $this->id), "item");?></li>
+            <li> <?php ActionLink::create("Friends", "Profile", "friends", NULL, array("id" => $this->id), "item");?></li>
+            <li> <?php ActionLink::create("Settings", "Profile", "settings", NULL, array("id" => $this->id), "item");?></li>            
         </ul>
 
         <?php echo $this->content; ?>
